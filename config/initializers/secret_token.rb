d0913611a15dda78d736d62fc7246cfc017b41d7
Amandeep <amandeep.bhamra@gmail.com>
2013-04-08 20:07:34 +0530

    will paginate implemented

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsbRc51::Application.config.secret_token = '69f62d453a0dbc1b2c862738571305707a830560875c36671783af5e892e43b9f7c516b6b92eff5e578b5e00290571f3cc707bdfc434081a5d63866429f5da2f'
